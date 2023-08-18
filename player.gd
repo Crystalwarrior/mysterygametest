46167cc86a344efc5a9d7b4406a877fc1dcbd7f2
Alex Noir <Varsash@gmail.com>
2023-08-18 19:51:29 +0300

    add sfx for knife swinging and stabbing someone block character direction when they're swinging the knife

--- player.gd
+++ player.gd
@@ -15,6 +15,8 @@ func get_input():
 	set_attacking(Input.is_action_just_pressed("attack"))
 	set_interacting(Input.is_action_just_pressed("interact"))
 	
+	if animation.current_animation == "KnifeSwing":
+		return
 	$DirectionPivot.rotation = snapped(global_position.direction_to(get_global_mouse_position()).angle(), PI/4)
 	var dir = int((snapped($DirectionPivot.rotation / PI, 0.01) + 1) * 4) + 2
 	$SpriteGroup.set_dir(8 - dir)
@@ -53,11 +55,18 @@ func swing_knife():
 
 
 func _on_swing_area_entered(area):
-	if area.get_parent() == self:
+	var parent = area.get_parent()
+	if parent == self:
 		return
-	var health = area.get_parent().get_node_or_null("HealthComponent")
+	var health = parent.get_node_or_null("HealthComponent")
 	if area.name == "HitBox" and health:
 		health.health -= 15
+		var sound = AudioStreamPlayer2D.new()
+		sound.stream = preload("res://assets/sounds/knife/stab1.wav")
+		sound.max_distance = 700
+		sound.connect("finished", sound.queue_free)
+		parent.add_child(sound)
+		sound.play()
 
 
 func _on_hit_box_area_entered(area):
